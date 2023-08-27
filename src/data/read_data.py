a78e2592461953d5382177f40c3add966d9631ff
jakub.prusakiewicz <jakub.prusakiewicz@silenteight.com>
2023-08-27 11:28:47 +0200

    initial commit

---
+++ src/data/read_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import json
+
+
+def read_data(path: str = "data/data.json") -> pd.DataFrame:
+    """ load your data here. This may look something like:
+        with open(path) as f:
+                data = json.load(f)
+            return pd.DataFrame(data["records"])
+    """
+    raise NotImplemented
